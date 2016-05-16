c8fc3d505d89b7369a722d9f87347ea63fdab3a1
daniel <daniel.cho.ih@gmail.com>
2016-05-16 14:55:18 +0900

    clubController 분산

--- bong/src/com/bong/club/ClubController2.java
+++ bong/src/com/bong/club/ClubController2.java
@@ -4,66 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller("bong.clubController")
-public class ClubController {
-	
-	@RequestMapping(value="/main/club")
-	public ModelAndView clubMain() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".layout.club.clubMain.봉다리 메인");
-		return mav;
-	}
-	
-	
-	
-	
-	@RequestMapping(value="/main/createClub")
-	public ModelAndView clubCreate() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".layout.club.open.동아리 개설하기");
-		return mav;
-	}
-	
-	@RequestMapping(value="/main/myClub")
-	public ModelAndView Myclub() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".four.club.dari.main.각 동아리 메인");
-		return mav;
-	}
-	
-	
-	@RequestMapping(value="/main/searchClub")
-	public ModelAndView clubSearch() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".layout.club.search.동아리 검색하기");
-		return mav;
-	}
+@Controller("bong.clubController2")
+public class ClubController2 {
 	
 	@RequestMapping(value="/club/index/main")
 	public ModelAndView myClubMain() throws Exception {
 		
 		ModelAndView mav = new ModelAndView(".four.club.dari.main.내 동아리 메인");
-		return mav;
-	}
-	
-	@RequestMapping(value="/club/index/vapply")
-	public ModelAndView clubVapply() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".four.club.dari.apply.article.봉다리 개인페이지");
-		return mav;
-	}
-	
-	@RequestMapping(value="/club/index/vapplyList")
-	public ModelAndView clubVapplyList() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".four.club.dari.apply.list.봉다리 개인페이지");
-		return mav;
-	}
-	
-	@RequestMapping(value="/club/index/japply")
-	public ModelAndView clubJapply() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".four.club.dari.japply.가입신청하기");
 		return mav;
 	}
 	
@@ -112,13 +59,6 @@ public class ClubController {
 		return mav;
 	}
 	/*개인동아리 자유게시판 끝*/
-	
-	@RequestMapping(value="/club/index/review")
-	public ModelAndView clubReview() throws Exception {
-		
-		ModelAndView mav = new ModelAndView(".four.club.dari.review.리뷰게시판");
-		return mav;
-	}
 	
 	@RequestMapping(value="/club/index/calendar")
 	public ModelAndView clubCalendar() throws Exception {
