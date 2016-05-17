4ab37e51de18818ef5fba6f1b1b4dfdf8f2eaab8
kdhhappy <kdh_happy@naver.com>
2016-05-17 19:06:38 +0900

    달력

--- bong/src/com/bong/admin/adminController.java
+++ bong/src/com/bong/admin/adminController.java
@@ -16,36 +16,37 @@ public class adminController {
 	@RequestMapping(value="/admin/member")
 	public ModelAndView adminMember() throws Exception {
 		
-		ModelAndView mav = new ModelAndView(".four.admin.member.멤버관리페이지");
+		ModelAndView mav = new ModelAndView(".admin4.main.member");
 		return mav;
 	}
 	
 	@RequestMapping(value="/admin/club")
 	public ModelAndView adminClub() throws Exception {
 		
-		ModelAndView mav = new ModelAndView(".four.admin.club.동아리관리페이지");
+		ModelAndView mav = new ModelAndView(".admin4.main.club");
 		return mav;
 	}
 	
 	@RequestMapping(value="/admin/demander")
 	public ModelAndView adminDemander() throws Exception {
 		
-		ModelAndView mav = new ModelAndView(".four.admin.demander.수요처 관리페이지");
+		ModelAndView mav = new ModelAndView(".admin4.main.demander");
 		return mav;
 	}
 	
 	@RequestMapping(value="/admin/approval")
 	public ModelAndView adminApproval() throws Exception {
 		
-		ModelAndView mav = new ModelAndView(".four.admin.approval.수요처_동아리 가입승인페이지");
+		ModelAndView mav = new ModelAndView(".admin4.main.approval");
 		return mav;
 	}
 	
-	@RequestMapping(value="/admin/tab")
-	public ModelAndView tab(
-			) throws Exception {
+	@RequestMapping(value="/admin/cal")
+	public ModelAndView adminCal() throws Exception {
 		
-		ModelAndView mav = new ModelAndView("/demander/dari/admin/volList");
+		ModelAndView mav = new ModelAndView(".admin4.calendar.calendar");
 		return mav;
 	}
+	
+	
 }
