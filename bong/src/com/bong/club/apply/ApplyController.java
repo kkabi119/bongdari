92764e3d94e9a8ef7a0c5f54d23e79ab562a11c9
sist <sist@sist-215>
2016-05-20 16:26:12 +0900

    ㅎ

---
+++ bong/src/com/bong/club/apply/ApplyController.java
@@ -0,0 +1,192 @@
+package com.bong.club.apply;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bong.common.FileManager;
+import com.bong.common.MyUtil;
+import com.bong.member.SessionInfo;
+
+
+@Controller("club.applyController")
+public class ApplyController {
+	
+	@Autowired
+	private ApplyService service;
+	
+	@Autowired
+	private MyUtil myUtil;
+	
+	
+	/*개인동아리 봉사 신청게시판*/
+	
+	///////////////////////////////////////////// 리스트 ///////////////////////////////////////////////////
+	@RequestMapping(value="club/index/vapplyList")
+	public ModelAndView clubApplyList(
+			HttpServletRequest req,
+			@RequestParam(value="page", defaultValue="1") int current_page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue
+			) throws Exception {
+		
+		String cp=req.getContextPath();
+		
+		int numPerPage   = 10;  // 한 화면에 보여주는 게시물 수
+		int total_page = 0;
+		int dataCount = 0;
+		
+		if(req.getMethod().equalsIgnoreCase("GET")) { // GET 방식인 경우
+			searchValue = URLDecoder.decode(searchValue, "utf-8");
+		}
+		
+		 // 전체 페이지 수
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("searchKey", searchKey);
+        map.put("searchValue", searchValue);
+                
+        dataCount = service.dataCount(map);
+        
+        System.out.println(dataCount);
+        if(dataCount != 0)
+            total_page = myUtil.pageCount(numPerPage,  dataCount) ;
+		
+        // 다른 사람이 자료를 삭제하여 전체 페이지수가 변화 된 경우
+        if(total_page < current_page) 
+            current_page = total_page;
+
+        // 리스트에 출력할 데이터를 가져오기
+        int start = (current_page - 1) * numPerPage + 1;
+        int end = current_page * numPerPage;
+        map.put("start", start);
+        map.put("end", end);
+                       
+        // 글 리스트
+        List<Apply> list = service.listApply(map);
+        
+        ////////////////// 현재 신청자수 가져오기
+        /*int applyNum; //현재 신청자수
+        Map<String, Object> map2 = new HashMap<String, Object>();
+       
+        map2.put("list",list);
+      
+        System.out.println(map2);
+        
+        applyNum=service.applyCount_club(map2); */
+             
+     // 리스트의 번호
+        int listNum, n = 0;
+        Iterator<Apply> it=list.iterator();
+        
+        while(it.hasNext()) {
+            Apply data = it.next();
+            listNum = dataCount - (start + n - 1);
+            
+            data.setListNum(listNum);   
+            System.out.println("야야야:" +data.getVolunIdx());         
+            System.out.println("listNum: "+listNum);
+            n++;
+        }
+        
+        String params = "";
+        String urlList = cp+"/club/index/apply/list";
+        String urlArticle = cp+"/club/index/apply/article?page=" + current_page;
+        if(searchValue.length()!=0) {
+        	params = "searchKey=" +searchKey + 
+        	             "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");	
+        }
+        
+        if(params.length()!=0) {
+            urlList = cp+"/club/index/apply/list?" + params;
+            urlArticle = cp+"/club/index/apply/article?page=" + current_page + "&"+ params;
+        }
+        
+		ModelAndView mav = new ModelAndView(".four.club.dari.apply.list.봉사신청");
+		
+        mav.addObject("list", list);
+    //    mav.addObject("applyNum",applyNum);
+        mav.addObject("urlArticle", urlArticle);
+        mav.addObject("page", current_page);
+        mav.addObject("dataCount", dataCount);
+        mav.addObject("total_page", total_page);
+        mav.addObject("paging", myUtil.paging(current_page, total_page, urlList));
+        
+		return mav;
+	}
+	
+	///////////////////////////////////////////// 글보기 ///////////////////////////////////////////////////
+	@RequestMapping(value="/club/index/apply/article")
+	public ModelAndView readClubApply(HttpSession session,
+			@RequestParam(value="num") int num,
+			@RequestParam(value="page") String page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue
+			) throws Exception {
+		
+		SessionInfo info=(SessionInfo)session.getAttribute("member");
+		if(info==null) {
+			return new ModelAndView("redirect:/member/login");
+		}
+
+		searchValue = URLDecoder.decode(searchValue, "utf-8");
+		
+		// 조회수 증가
+		service.updateHitCount(num);
+
+		// 해당 레코드 가져 오기
+		Apply dto = service.readApply(num);
+
+		if(dto==null)
+			return new ModelAndView("redirect:.club.index.notice.list?page="+page);
+		
+		// 전체 라인수
+        // int linesu = dto.getContent().split("\n").length;
+		
+		// 스마트에디터를 사용하므로 엔터를 <br>로 변경하지 않음
+        // dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+        
+		// 이전 글, 다음 글
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchKey", searchKey);
+		map.put("searchValue", searchValue);
+		map.put("num", num);
+
+		Apply preReadDto = service.preReadNotice(map);
+		Apply nextReadDto = service.nextReadNotice(map);
+        
+		String params = "page="+page;
+		if(searchValue.length()!=0) {
+		    params += "&searchKey=" + searchKey + 
+		                    "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");
+		}
+		
+		ModelAndView mav = new ModelAndView(".four.club.dari.apply.article.봉사신청 글보기");
+		mav.addObject("dto", dto);
+		mav.addObject("preReadDto", preReadDto);
+		mav.addObject("nextReadDto", nextReadDto);
+
+		mav.addObject("page", page);
+		mav.addObject("params", params);
+		return mav;
+	}
+	
+}
+	
+	
---
+++ bong/src/com/bong/club/apply/ApplyService.java
@@ -0,0 +1,28 @@
+package com.bong.club.apply;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ApplyService {
+	public List<Apply> listApply(Map<String, Object> map);
+	public int dataCount(Map<String, Object> map);
+	
+	public int applyCount_club(Map<String, Object> map);
+	public int applyCount(Map<String, Object> map);
+
+	public Apply readApply(int num);
+	public int updateHitCount(int num);
+	public Apply preReadNotice(Map<String, Object> map);
+	public Apply nextReadNotice(Map<String, Object> map);
+	public int updateNotice(Apply dto, String path);
+	public int deleteNotice(int num, String saveFilename, String path);
+	
+	public int deleteNoticeId(String userId, String root);
+	
+	public int insertReply(Reply dto);
+	public List<Reply> listReply(Map<String, Object> map);
+	public List<Reply> listReplyAnswer(int answer);
+	public int replyDataCount(Map<String, Object> map);
+	public int replyCountAnswer(int answer);
+	public int deleteReply(Map<String, Object> map);
+}
