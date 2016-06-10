2408f2927a0848b611fb4538f259864cf680760a
sist <sist@sist-215>
2016-06-10 20:03:51 +0900

    ㅎㅎㅎ

---
+++ bong/src/com/bong/club/JoinClub.java
@@ -0,0 +1,29 @@
+package com.bong.club;
+
+public class JoinClub {
+
+	private int joinCount;
+	private String memAddr;
+	private String authority;
+	
+	public int getJoinCount() {
+		return joinCount;
+	}
+	public void setJoinCount(int joinCount) {
+		this.joinCount = joinCount;
+	}
+	public String getMemAddr() {
+		return memAddr;
+	}
+	public void setMemAddr(String memAddr) {
+		this.memAddr = memAddr;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+	
+}
